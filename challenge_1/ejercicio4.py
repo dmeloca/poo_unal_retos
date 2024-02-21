ad8c71ee76782aafb1d828e0b235f0c96c9de10a
Daniel Melo <115378192+estfloyd@users.noreply.github.com>
2024-02-21 15:20:03 -0500

    Finishing the algorithm
    Changes:
    1. Doing the sumatory function
    2. Fixing the selection of arguments

--- challenge_1/ejercicio4.py
+++ challenge_1/ejercicio4.py
@@ -1,13 +1,18 @@
 def ask_user(question: str) -> str:
     return input(question)
 
+def check_sumatory(numbers: list) -> int:
+    max_sum: int = numbers[1] + numbers[2]
+    for i in range(len(numbers)-1):
+        if (numbers[i] + numbers[i+1]) > max_sum:
+            max_sum = numbers[i] + numbers[i+1]
+    return max_sum
+
+
 def main():
     number_str = ask_user("[?] Ingrese los números que desea verificar: [Ejemplo:1 2 3 4 5 6 7] ")
     number_list = [int(x) for x in number_str.split() if x != " "]
-    number_list.sort()
-    x: int = number_list[-1]
-    y: int = number_list[-2]
-    print(f"La suma de los enteros más grandes es: {x+y}")
+    print(f"La suma de los enteros más grandes es: {check_sumatory(number_list)}")
 
 if __name__ == "__main__":
     main()
