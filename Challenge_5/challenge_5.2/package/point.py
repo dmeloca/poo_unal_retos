613d368d2af743595440beeb4010aae94213c61f
Daniel Melo <115378192+estfloyd@users.noreply.github.com>
2024-04-08 00:06:03 -0500

    removing an error

--- Challenge_5/challenge_5.2/package/point.py
+++
@@ -1,7 +0,0 @@
-class Point:
-    def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
-    
-    def compute_distance(self, point):
-        return (((point.x - self.x)**2)+((point.y - self.y)**2))**(1/2)
--- Challenge_5/challenge_5.2/package/scalene.py
+++
@@ -1,9 +0,0 @@
-import package.triangle as triangle
-
-class Scalene(triangle.Triangle):
-    def __init__(self, vertices: list, edges: list, inner_angles: list, is_regular: bool = True):
-        super().__init__(vertices, edges, inner_angles, is_regular)
-
-    def compute_area(self):
-        semi_perimeter = self.compute_perimeter()/2
-        return (semi_perimeter*(semi_perimeter - self.edges[0])*(semi_perimeter - self.edges[1])*(semi_perimeter -self.edges[2]))**(1/2)
